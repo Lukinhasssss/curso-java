cda0434b553a812b971a265cf1ac4d4d928bfaf3
Lucas Monteiro <lukasmonteir0@hotmail.com>
2020-11-29 09:44:17 -0300

    last commit

--- composicao/src/application/Program.java
+++ composicao/src/application/Program.java
@@ -1,8 +1,19 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		/*
 		 * Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário).
@@ -10,7 +21,55 @@ public class Program {
 		 * funcionário nesse mês.
 		 */
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
+		
+		System.out.print("Enter department name: ");
+		String departmentName = sc.nextLine();
+		System.out.println("Enter worker data:");
+		System.out.print("Name: ");
+		String workerName = sc.nextLine();
+		System.out.print("Level: ");
+		String workerLevel = sc.nextLine();
+		System.out.print("Base salary: ");
+		double baseSalary = sc.nextDouble();
+		
+		// Com base nos dados acima é possível instanciar um trabalhador
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+		
+		System.out.print("How many contracts to this worker? ");
+		int n = sc.nextInt();
+		
+		// Criando um for para ler os dados dos contratos
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter contract #" + i + " data:");
+			System.out.print("Date (DD/MM/YYYY): ");
+			Date contractDate = sdf.parse(sc.next());
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			System.out.print("Duration (hours): ");
+			int hours = sc.nextInt();
+			
+			// Com os dados acima já tenho condições de instanciar um novo contrato
+			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+			
+			// Associando o contract com o worker
+			worker.addContract(contract);
+		}
+		
+		System.out.println();
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+		
+		
+		sc.close();
 
 	}
 
