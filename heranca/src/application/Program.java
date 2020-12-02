49f72d7908979f3bc79652f47c87d7de5c8ba718
Lucas Monteiro <lukasmonteir0@hotmail.com>
2020-12-02 20:07:13 -0300

    last commit

---
+++ heranca/src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Herança é um tipo de associação que permite que uma classe herde
+		 * todos os dados e comportamentos (atributos e métodos) de outra.
+		 * 
+		 * 		Exemplo:
+		 * 	Suponha um negócio de banco que possui uma conta comum e uma conta
+		 * 	para empresas, sendo que a conta para empresa possui todos os
+		 * 	membros da conta comum, mais um limite de empréstimo e uma operação
+		 * 	de realizar empréstimo.
+		 */
+		
+		
+
+	}
+
+}
