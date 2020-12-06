182d4507a864576bdb4755a81fa84e1e6b76c241
Lucas Monteiro <lukasmonteir0@hotmail.com>
2020-12-06 11:53:02 -0300

    last commit

--- heranca_upcasting_e_downcasting/src/application/Program.java
+++ heranca_upcasting_e_downcasting/src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
@@ -21,7 +23,32 @@ public class Program {
 		 * Upcasting e Downcasting
 		 */
 		
+		@SuppressWarnings("unused")
 		Account account = new Account(1001, "Alex", 0.0);
+		BusinessAccount businessAccount = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		
+		// UPCASTING
+		@SuppressWarnings("unused")
+		Account account1 = businessAccount;
+		Account account2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account account3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		
+		// DOWNCASTING
+		BusinessAccount account4 = (BusinessAccount)account2;
+		account4.loan(100.0);
+		
+		// BusinessAccount account5 = (BusinessAccount)account3; // Neste caso dará erro de execução
+		if (account3 instanceof BusinessAccount) {
+			BusinessAccount account5 = (BusinessAccount)account3;
+			account5.loan(200.0);
+			System.out.println("Loan!"); // Loan == empréstimo
+		}
+		
+		if (account3 instanceof SavingsAccount) {
+			SavingsAccount account5 = (SavingsAccount)account3;
+			account5.updateBalance();
+			System.out.println("Update!");
+		}
 
 	}
 
