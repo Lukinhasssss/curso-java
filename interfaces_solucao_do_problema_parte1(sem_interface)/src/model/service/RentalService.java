8dcd1e8b9421107ecfc3f5240dd8f888dc3c818c
Lucas Monteiro <lukasmonteir0@hotmail.com>
2020-12-21 10:00:46 -0300

    last commit

---
+++ interfaces_solucao_do_problema_parte1(sem_interface)/src/model/service/RentalService.java
@@ -0,0 +1,37 @@
+package model.service;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
+	
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		super();
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime(); // Pega o valor em milissegundos da data
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2 - t1) / 1000 / 60 / 60; // Acha a difença em milissegundos, / 1000 converte para segundos, / 60 converte para minutos e /60 converte para hora
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour; // Math.ceil arredonda o valor para cima
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay; // (hours / 24) para achar a quantidade em dias
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+
+}
